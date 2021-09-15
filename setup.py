7d2b42f7c4f95f513cc57bfedf77b1e6e42b493d
Inada Naoki <songofacandy@gmail.com>
2021-09-15 16:48:02 +0900

    v0.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-with open("README.md", "rt") as f:
+with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="myloginpath",
     license="MIT",
-    version="0.0.2",
+    version="0.0.3",
     description="MySQL login path file reader",
     long_description=readme,
     long_description_content_type="text/markdown",
